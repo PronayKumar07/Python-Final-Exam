59bce1a67475bd87d803b42ac44e5b5b993f2211
PronayKumar07 <129740036+PronayKumar07@users.noreply.github.com>
2023-12-30 13:54:09 +0600

    Update Bank_Management_System.py

--- Bank_Management_System.py
+++ Bank_Management_System.py
@@ -7,25 +7,26 @@ class Bank:
         self.available_balance = 0
         self.loan_feature = 1
         self.bankrupt = 1
-        
 
     def create_account(self):
         name = input("Enter Your Name: ")
         email = input("Enter Your Email: ")
         address = input("Enter Your Address: ")
         account_type = input("Enter Your Account_Type: Savings or Current: ")
+        password = input("Set Your Password: ")
 
         account_number = random.randint(100, 999)
         self.accounts[account_number] = {
             'name' : name,
             'email' : email,
+            'password' : password,
             'address' : address,
             'account_type' : account_type,
             'balance' : 0,
             'transactions' : [],
             'loan_taken': 0
         }
-        print(f'Your Account created Successfully and Account Number {account_number}')
+        print(f'Your Account created Successfully and Account Number {account_number}, Password: {password}')
 
     
     def deposit(self, account_number, amount):
@@ -134,94 +135,124 @@ class Bank:
     def loan_feature_on_of(self, loan_on_of):
         self.loan_feature = loan_on_of
 
+    def valid_account_check(self, acc_no, passw):
+        if acc_no in self.accounts and passw == self.accounts[acc_no]['password']:
+            return 1
+        else:
+            return 0
 
 bank = Bank()
 
-    
-
+Admin_Account = {'Id': '1001','Name' : "Pronay", 'Address': 'Khulna', 'Password' : '1234'}
+print('Admin Info: ', Admin_Account)
 
 
 while(True):
     print('-----------------------------------------------')
     print('-----------------------------------------------')
-    ch = int(input('Choose Option: 1: User /// 2: Admin:  '))
+    ch = int(input('Choose Option For Log In: 1: Admin: /// 2: User:  '))
+
     if ch == 1:
-        while(True):
-            print('-----------------------------------------------')
-            print('-----------------------------------------------')
-            print('Now You Are User Login /// Press 0 To Log Out:')
-            print('Choose Option:')
-            print('1.Create Account.')
-            print('2.Deposit.')
-            print('3.Withdraw.')
-            print('4.Check Balance.')
-            print('5.Transaction History.')
-            print('6.Take Loan.')
-            print('7.Balance Transfer.')
+        id = input('Enter Admin Id: ')
+        password = input('Enter Admin Password: ')
 
-            c = int(input())
-            if c == 0:
-                break
-            if c == 1:
-                bank.create_account()
-            elif c == 2:
-                account_no = int(input('Enter Account Number: '))
-                amount = int(input('Enter Amount: '))
-                bank.deposit(account_no, amount)
-            elif c == 3:
-                account_no = int(input('Enter Account Number: '))
-                amount = int(input('Enter Amount: '))
-                bank.withdraw(account_no, amount)
-            elif c == 4:
-                account_no = int(input('Enter Account Number: '))
-                bank.check_balance(account_no)
-            elif c == 5:
-                account_no = int(input('Enter Account Number: '))
-                bank.transaction_history(account_no)
-            elif c == 6:
-                account_no = int(input('Enter Account Number: '))
-                amount = int(input('Enter Amount: '))
-                bank.take_loan(account_no, amount)                
-            elif c == 7:
-                f_account = int(input('Enter Sending Account Number: '))
-                t_account = int(input('Enter Receiving Account Number: '))
-                amount = int(input('Enter Amount: '))
-                bank.transfer_amount(f_account, t_account, amount)
+        if(id == Admin_Account['Id'] and password == Admin_Account['Password']):
+
+            print('-----------------------------------------------')
+            print('-----------------------------------------------')
+            while(True):        
+                print('-----------------------------------------------')
+                print('-----------------------------------------------')
+                print('Now You Are Admin Login /// Press 0 To Log Out:')
+                print('Choose Option:')
+                print('1.Create Account.')
+                print('2.Delete Account.')
+                print('3.See User List.')
+                print('4.Total Balance Of Bank.')
+                print('5.Total Loan.')
+                print('6.Control Loan Feature.')
+                print('7.Control Bankrupt Feature.')
+
+                c = int(input())
+                if c == 0:
+                    break
+                if c == 1:
+                    bank.create_account()
+                elif c == 2:
+                    account_no = int(input('Enter Account Number: '))
+                    bank.delete_account(account_no)
+                elif c == 3:
+                    bank.see_all_account()
+                elif c == 4:
+                     bank.available_balance_cheak()
+                elif c == 5:
+                    bank.loan_check()
+                elif c == 6:
+                    on_of = int(input('Enter 0 for off loan system: '))
+                    bank.loan_feature_on_of(on_of)
+                    print('Loan System Turn Off Successfully')
+                elif c == 7:
+                    on_of = int(input('Enter 0 for Declare Bankrupt: '))
+                    bank.is_bankrupt(on_of)
+                    print('Declare Bankrupt option Successfully')
+            
+        else:
+            print('Wrong Id or Password...')
+
+
     elif ch == 2:
-        print('-----------------------------------------------')
-        print('-----------------------------------------------')
-        while(True):        
-            print('-----------------------------------------------')
-            print('-----------------------------------------------')
-            print('Now You Are Admin Login /// Press 0 To Log Out:')
-            print('Choose Option:')
-            print('1.Create Account.')
-            print('2.Delete Account.')
-            print('3.See User List.')
-            print('4.Total Balance Of Bank.')
-            print('5.Total Loan.')
-            print('6.Control Loan Feature.')
-            print('7.Control Bankrupt Feature.')
+        print('Enter option 1.Create Account....   2.Log In...  ')
+        op = int(input())
+        if op == 1:
+            print('Please Enter Valid Information for Create Account...')
+            bank.create_account()
 
-            c = int(input())
-            if c == 0:
-                break
-            if c == 1:
-                bank.create_account()
-            elif c == 2:
-                account_no = int(input('Enter Account Number: '))
-                bank.delete_account(account_no)
-            elif c == 3:
-                bank.see_all_account()
-            elif c == 4:
-                bank.available_balance_cheak()
-            elif c == 5:
-                bank.loan_check()
-            elif c == 6:
-                on_of = int(input('Enter 0 for off loan system: '))
-                bank.loan_feature_on_of(on_of)
-                print('Loan System Turn Off Successfully')
-            elif c == 7:
-                on_of = int(input('Enter 0 for Declare Bankrupt: '))
-                bank.is_bankrupt(on_of)
-                print('Declare Bankrupt option Successfully')
+        elif op == 2:
+            ac_no = int(input('Enter Account Number: '))
+            Pass = input('Enter Password: ')
+
+            if(bank.valid_account_check(ac_no, Pass) == 1):
+
+                while(True):
+                    print('-----------------------------------------------')
+                    print('-----------------------------------------------')
+                    print('Now You Are User Login /// Press 0 To Log Out:')
+                    print('Choose Option:')
+                    print('1.Deposit.')
+                    print('2.Withdraw.')
+                    print('3.Check Balance.')
+                    print('4.Transaction History.')
+                    print('5.Take Loan.')
+                    print('6.Balance Transfer.')
+
+                    c = int(input())
+                    if c == 0:
+                        break
+                    elif c == 1:
+                        account_no = int(input('Enter Account Number: '))
+                        amount = int(input('Enter Amount: '))
+                        bank.deposit(account_no, amount)
+                    elif c == 2:
+                        account_no = int(input('Enter Account Number: '))
+                        amount = int(input('Enter Amount: '))
+                        bank.withdraw(account_no, amount)
+                    elif c == 3:
+                        account_no = int(input('Enter Account Number: '))
+                        bank.check_balance(account_no)
+                    elif c == 4:
+                        account_no = int(input('Enter Account Number: '))
+                        bank.transaction_history(account_no)
+                    elif c == 5:
+                        account_no = int(input('Enter Account Number: '))
+                        amount = int(input('Enter Amount: '))
+                        bank.take_loan(account_no, amount)                
+                    elif c == 6:
+                        f_account = int(input('Enter Sending Account Number: '))
+                        t_account = int(input('Enter Receiving Account Number: '))
+                        amount = int(input('Enter Amount: '))
+                        bank.transfer_amount(f_account, t_account, amount)
+            else:
+                print('Wrong Account Number or Password...')
+
+
+
